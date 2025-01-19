0c9884c9a656942b540fcec7b287a70db2f31659
JD <joseignaciodilonemps@gmail.com>
2025-01-18 20:30:24 -0400

    first commit, again

---
+++ begin.py
@@ -0,0 +1,21 @@
+# Autor: Jose Dilone
+# Matricula: 21-0838
+# ♣ Asignacion 1 ♣
+
+def load_Page():
+    print("Loading...")
+
+def space_Site():
+    print("\n______________________________\n")
+
+name = input("Inserta nombre: ")
+load_Page()
+lastName = input("Inserta apellido: ")
+load_Page()
+age = int(input("Inserta edad: "))
+load_Page()
+space_Site()
+print(name)
+print(lastName)
+print(age)
+
